59899abd05dc14f30cc1e09df97d724c59e79266
msweeneydev <matthewsweeney@mailup.net>
2018-12-20 23:32:56 +0000

    minor config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,33 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <SEO
+      title="Baggage"
+      keywords={[
+        `gatsby`,
+        `application`,
+        `react`,
+        'baggage',
+        'luggage',
+        'airport',
+      ]}
+    />
+    <div
+      style={{
+        background: '#111',
+        display: 'grid',
+        height: '100vh',
+        overflow: 'auto',
+        placeContent: 'center center',
+        // width: `100vw`,
+      }}
+    >
+      <h1 style={{ color: 'white', fontSize: '5em' }}>Baggage.</h1>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
